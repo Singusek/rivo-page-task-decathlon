68edd7e86778a2650971dcba932b23f89677097b
Singusek <anita0052@gmail.com>
2023-09-20 17:35:45 +0200

    Fetch

---
+++ main.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const productContainer = document.getElementById("product-container");
+    const seeAllButton = document.querySelector(".btn.btn-light");
+
+    let loadedProducts = 3;
+  
+    async function fetchProducts() {
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products/category/women's clothing"
+        );
+        const products = await response.json();
+  
+        for (let i = 0; i < 3; i++) {
+          const product = products[i];
+          const card = createProductCard(product);
+          productContainer.appendChild(card);
+        }
+      } catch (error) {
+        console.error("Błąd podczas pobierania danych z API", error);
+      }
+    }
+  
+    function createProductCard(product) {
+      const card = document.createElement("div");
+      card.classList.add("col");
+      card.innerHTML = `
+        <div class="card h-100">
+          <img src="${product.image}" class="card-img-top" alt="${product.title}">
+          <div class="card-body">
+            <h5 class="card-title">${product.title}</h5>
+            <p>$${product.price} | ${product.rating.rate.toFixed(1)}
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.950l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+              </svg>
+            </p>
+          </div>
+        </div>
+      `;
+      return card;
+    }
+  
+    fetchProducts();
+  });
